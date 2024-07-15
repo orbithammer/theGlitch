5e97ca67eb7a1722d4b61b94bbb36fdbf7402dcb
Brock Penner <orbithammer@users.noreply.github.com>
2024-07-15 18:16:07 +0900

    Update contact.tsx

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -2,17 +2,20 @@ import React from "react";
 
 const ContactPage: React.FC = () => {
     return (
-        <form name="contact" data-netlify={true}>
-        <p>
-            <label>Name <input type="text" name="name" /></label>
-        </p>
-        <p>
-            <label>Email <input type="email" name="email" /></label>
-        </p>
-        <p>
-            <button type="submit">Send</button>
-        </p>
-    </form>
+        <>
+            <h3 style={{ color: "red" }}>Under Construction</h3>
+            <form name="contact" data-netlify={true}>
+                <p>
+                    <label>Name <input type="text" name="name" /></label>
+                </p>
+                <p>
+                    <label>Email <input type="email" name="email" /></label>
+                </p>
+                <p>
+                    <button type="submit">Send</button>
+                </p>
+            </form>
+        </>
     );
 };
 
